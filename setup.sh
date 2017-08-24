c28ffb748419718b672c8b82cbeeb147c8caaef4
Zeki Unal <zekiunal@gmail.com>
2017-08-24 15:18:40 +0300

    .

--- setup.sh
+++ setup.sh
@@ -68,10 +68,10 @@ database_up() {
 
 web_build() {
     echo "Portus Web Building"
-    cp config/Dockerfile portus/Dockerfile
+    #cp config/Dockerfile portus/Dockerfile
     cp $portus_config_file portus/config/config.yml
-    #cd portus && docker build --no-cache -t ${web_container} .
-    cd portus && docker build -t ${web_container} .
+    cd portus && docker build --no-cache -t ${web_container} .
+    #cd portus && docker build -t ${web_container} .
     cd /portus_setup
 }
 
